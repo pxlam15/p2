baa622b51e8248c98f937eb5cd5ef59154656ede
Peter Lam <pxlam15@gmail.com>
2014-10-02 12:26:09 -0400

    added css, improved functionality (it works!), README.md updated, index.php updated, added image folder

--- Logic/logic.php
+++ Logic/logic.php
@@ -2,26 +2,26 @@
 
 # passing form data to use for logic
 $numWords = $_POST['num']; 
-$boolNum = $_POST['numYes'];
-$boolSym = $_POST['symYes'];
+#$boolNum = $_POST['numYes'];
+#$boolSym = $_POST['symYes'];
 
 # Hardcoded array list for password 
 $wordList = array(
-	'a', 
-	'b', 
-	'c', 
-	'd', 
-	'e', 
-	'f', 
-	'g', 
-	'h', 
-	'i', 
-	'j', 
-	'k', 
-	'l', 
-	'm', 
-	'n', 
-	'o');
+	'kelly', 
+	'pajamas', 
+	'shrimp', 
+	'million', 
+	'charizard', 
+	'master', 
+	'ignis', 
+	'awakening', 
+	'stratosphere', 
+	'odin', 
+	'eye', 
+	'professor', 
+	'indicted', 
+	'vex', 
+	'weak');
 $symList = array("!", "@", "#", "$", "%", "^", "&", "*", "(", ")");
 
 $result = array();
@@ -36,32 +36,4 @@ shuffle($result);
 shuffle($symList);
 
 
-for($i = 0; $i < $numWords; $i++){
-	echo $result[$i] . "-";
-}	
 
-if(isset($_POST['numYes'])){
-	$r1 = rand(0, 9);
-	echo $r1;
-}
-
-if(isset($_POST['symYes'])){
-	$r2 = rand(0, 9);
-	echo $symList[$r2];
-}
-
-
-
-/*
-	if (isset($_POST['Numbers with Password?']) && $$_POST['Numbers with Password?'] == '1'){
-		$r1 = rand(0, 9);
-		echo "I work";
-	} 
-
-	if (isset($boolSym) && $boolSym == '1'){
-		$r2 = rand(0, 9);
-		echo "I work 2";
-	}
-	*/
-
-#print_r($result);
